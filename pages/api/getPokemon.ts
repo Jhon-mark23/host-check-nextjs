e4beffd7fb0e2016f98457c73d5a874afa42464d
Mark <123864853+Jhon-mark23@users.noreply.github.com>
2025-02-15 12:42:59 +0800

    Update getPokemon.ts

--- pages/api/getPokemon.ts
+++ pages/api/getPokemon.ts
@@ -1,5 +1,6 @@
 import { Pokemon } from "@/types/pokemon";
 import type { NextApiRequest, NextApiResponse } from "next";
+export const runtime = 'edge';
 
 export default async function handler(
     req: NextApiRequest,
